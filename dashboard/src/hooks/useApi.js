98783588f0073463fc2a411d63904197acbcd36f
99mitch <mehditej94@gmail.com>
2026-03-25 17:24:06 +0700

    feat: add Axios API layer with X-Admin-Key interceptor

---
+++ dashboard/src/hooks/useApi.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: '/api/admin',
+})
+
+api.interceptors.request.use((config) => {
+  const key = localStorage.getItem('adminKey')
+  if (key) {
+    config.headers['X-Admin-Key'] = key
+  }
+  return config
+})
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('adminKey')
+      window.location.href = '/login?error=1'
+    }
+    return Promise.reject(error)
+  }
+)
+
+export default api
